62ecd06862b340929249c574ae9f9b0012c43346
shardul1501 <shardulshroff936@gmail.com>
2020-07-25 18:33:24 +0530

    Image Storage Issue solved

--- Table_create.php
+++ Table_create.php
@@ -7,7 +7,7 @@
 	if (!$conn) {
 		die("Connection failed:" . mysqli_connect_error());
 	}
-	// $sql = "CREATE TABLE candidate_visible (start_time TIME NOT NULL, end_time TIME  NOT NULL, title VARCHAR(50) NOT NULL, reg_link VARCHAR(200) NOT NULL, event_desc VARCHAR(200) NOT NULL, image VARCHAR(200) NOT NULL, opt_link VARCHAR(200))";
+	// $sql = "CREATE TABLE candidate_visible (start_time TIME NOT NULL, end_time TIME  NOT NULL, title VARCHAR(50) NOT NULL, reg_link VARCHAR(200) NOT NULL, event_desc VARCHAR(200) NOT NULL, image LONGBLOB NOT NULL, opt_link VARCHAR(200))";
 	$sql = "CREATE TABLE registration (username VARCHAR(30) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(50) NOT NULL)";
 	if (mysqli_query($conn, $sql)) {
 		echo "Table created";
