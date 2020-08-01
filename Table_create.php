7ae4ac813bfdec667d75bf9cdcb679ab2bf43e6b
shardul1501 <shardulshroff936@gmail.com>
2020-08-01 15:50:17 +0530

    Table query changed

--- Table_create.php
+++ Table_create.php
@@ -8,7 +8,8 @@
 		die("Connection failed:" . mysqli_connect_error());
 	}
 	// $sql = "CREATE TABLE candidate_visible (start_time TIME NOT NULL, end_time TIME  NOT NULL, title VARCHAR(50) NOT NULL, reg_link VARCHAR(200) NOT NULL, event_desc VARCHAR(200) NOT NULL, image LONGBLOB NOT NULL, opt_link VARCHAR(200))";
-	$sql = "CREATE TABLE registration (username VARCHAR(30) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(50) NOT NULL)";
+	// $sql = "CREATE TABLE registration (username VARCHAR(30) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(50) NOT NULL)";
+	$sql = "CREATE TABLE commitee_head_login (username VARCHAR(80) NOT NULL , password VARCHAR(80) NOT NULL , email VARCHAR(80) NOT NULL , committee VARCHAR(10) NOT NULL , PRIMARY KEY (username)) ENGINE = InnoDB";
 	if (mysqli_query($conn, $sql)) {
 		echo "Table created";
 	}
